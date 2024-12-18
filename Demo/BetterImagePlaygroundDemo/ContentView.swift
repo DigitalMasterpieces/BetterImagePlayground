54748b178f195ea7fa03d5e9102d483b30ee7cbe
Frank Rupprecht <frank@digitalmasterpieces.com>
2024-12-18 16:21:31 +0100

    Added button to show the default image playground.

--- Demo/BetterImagePlaygroundDemo/ContentView.swift
+++ Demo/BetterImagePlaygroundDemo/ContentView.swift
@@ -1,17 +1,23 @@
 import SwiftUI
-
+import ImagePlayground
 import BetterImagePlayground
 
 
 struct ContentView: View {
+
+    @State var showDefaultImagePlayground = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: { self.showDefaultImagePlayground.toggle() }) {
+                Label("Default Image Playground", systemImage: "apple.image.playground")
+            }
         }
+        .buttonStyle(.borderedProminent)
         .padding()
+        .imagePlaygroundSheet(isPresented: self.$showDefaultImagePlayground) { url in
+            print("Image generated: \(url)")
+        }
     }
 }
 
