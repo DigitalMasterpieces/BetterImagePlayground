07e46bf0f142c38e571e82de8529fba4d618d422
Frank Rupprecht <frank@digitalmasterpieces.com>
2024-12-19 15:20:24 +0100

    Added new button for the better Image Playground to the demo project.

--- Demo/BetterImagePlaygroundDemo/ContentView.swift
+++ Demo/BetterImagePlaygroundDemo/ContentView.swift
@@ -6,16 +6,24 @@ import BetterImagePlayground
 struct ContentView: View {
 
     @State var showDefaultImagePlayground = false
+    @State var showBetterImagePlayground = false
 
     var body: some View {
         VStack {
             Button(action: { self.showDefaultImagePlayground.toggle() }) {
                 Label("Default Image Playground", systemImage: "apple.image.playground")
             }
+            Button(action: { self.showBetterImagePlayground.toggle() }) {
+                Label("Better Image Playground", systemImage: "wand.and.sparkles")
+            }
+            .tint(.orange)
         }
         .buttonStyle(.borderedProminent)
         .padding()
         .imagePlaygroundSheet(isPresented: self.$showDefaultImagePlayground) { url in
+            print("Image generated: \(url)")
+        }
+        .betterImagePlaygroundSheet(isPresented: self.$showBetterImagePlayground) { url in
             print("Image generated: \(url)")
         }
     }
