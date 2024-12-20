709e895267fd591e5088cbdc853cc25f50d2711e
Frank Rupprecht <frank@digitalmasterpieces.com>
2024-12-20 12:18:47 +0100

    Removed restrictive platform definition.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "BetterImagePlayground",
-    platforms: [.iOS(.v18), .macOS(.v15), .macCatalyst(.v18)],
     products: [
         .library(
             name: "BetterImagePlayground",
